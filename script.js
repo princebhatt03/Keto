e7e9998716e5d1f09795aff7f0dafddadebcbfa4
Prince Bhatt <princebhatt316@gmail.com>
2024-03-10 17:37:59 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// Image Swipper
+
+var swiper = new Swiper('.mySwiper', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// Hamburger Menu
+
+const hamburger = document.querySelector('.hamMenu');
+const navMenu = document.querySelector('.nav2 ul');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+const navLink = document.querySelectorAll('.nav2 a');
+
+navLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
